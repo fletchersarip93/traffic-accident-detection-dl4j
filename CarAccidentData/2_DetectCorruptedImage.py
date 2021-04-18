6b3be1f6b20f81c8df6816bca1d99de689a50a59
HeapShengOoi <heapsheng_ooi@yahoo.com>
2021-04-18 21:46:26 +0800

    Added Python script to check for corrupted file.
    Added gitignore to ignore jpg files under the OriData folder.

---
+++ CarAccidentData/2_DetectCorruptedImage.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import os
+import pandas as pd
+import numpy as np
+
+folderToUse = np.unique( pd.read_csv("OriData/lable.csv", dtype={"DirID":object})["DirID"].values )
+print(folderToUse)
+
+directory = "OriData/extracted_frames/"
+
+for foldername in folderToUse:
+#for foldername in os.listdir(directory):
+        folderN = os.path.join(directory, foldername)
+
+        for filename in os.listdir(folderN):
+                f = os.path.join(folderN, filename)
+                 
+                if os.path.isfile(f):
+                        try:
+                                if filename.split(".")[-1] == "jpg":
+                                        im = Image.open(f)
+                                else:
+                                        print("redundant " + foldername + "/" + filename)
+                        except:
+                                print("CORRUPTED " + foldername + "/" + filename)
+                else:
+                        print("UNKNOWN " + filename)
