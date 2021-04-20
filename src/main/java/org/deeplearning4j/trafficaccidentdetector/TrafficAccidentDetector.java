4ce5c5c116a810b9f1176f99a6162d8e88b0f06f
Fletcher Sarip <fletchersarip@gmail.com>
2021-04-20 14:48:00 +0800

    update the "from scratch" CNN+LSTM model

--- src/main/java/org/deeplearning4j/trafficaccidentdetector/TrafficAccidentDetector.java
+++ src/main/java/org/deeplearning4j/trafficaccidentdetector/TrafficAccidentDetector.java
@@ -1,18 +1,24 @@
 package org.deeplearning4j.trafficaccidentdetector;
 
 import org.apache.commons.io.FileUtils;
+import org.deeplearning4j.core.storage.StatsStorage;
 import org.deeplearning4j.nn.api.Model;
 import org.deeplearning4j.nn.conf.*;
+import org.deeplearning4j.nn.conf.inputs.InputType;
 import org.deeplearning4j.nn.conf.layers.*;
 import org.deeplearning4j.nn.conf.preprocessor.CnnToFeedForwardPreProcessor;
 import org.deeplearning4j.nn.conf.preprocessor.FeedForwardToRnnPreProcessor;
 import org.deeplearning4j.nn.conf.preprocessor.RnnToCnnPreProcessor;
 import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.nn.graph.util.ComputationGraphUtil;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.transferlearning.FineTuneConfiguration;
 import org.deeplearning4j.nn.transferlearning.TransferLearning;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.ui.api.UIServer;
+import org.deeplearning4j.ui.model.stats.StatsListener;
+import org.deeplearning4j.ui.model.storage.InMemoryStatsStorage;
 import org.deeplearning4j.zoo.ZooModel;
 import org.deeplearning4j.zoo.model.ResNet50;
 import org.nd4j.common.io.ClassPathResource;
@@ -36,21 +42,22 @@ import java.util.Map;
 
 public class TrafficAccidentDetector {
 
-    private static final int trainMaxIndex = 2;
-    private static final int testMaxIndex = 4;
+    private static final int trainMaxIndex = 130;
+    private static final int testMaxIndex = 154;
 
-    public static final int V_WIDTH = 224;
-    public static final int V_HEIGHT = 224;
+    public static final int V_WIDTH = 130;
+    public static final int V_HEIGHT = 130;
     public static final int nChannels = 3;
 
     // Note that you will need to run with at least 7G off heap memory
     // if you want to keep this batchsize and train the nn config specified
     private static final int miniBatchSize = 1;
-    private static final int seed = 1234;
-    private static final double learningRate = 0.0001;
-    private static final double l2Coeff = 0.001;
+//    private static final int seed = 560;
+    private static final double learningRate = 0.00001;
+    private static final double l2Coeff = 0.0001;
     private static final int truncatedBPTTLength = 50;
-    private static final int nTrainEpochs = 15;
+    private static final int nTrainEpochs = 300;
+    private static final int FRAME_LENGTH = 361;
 
     public static void main(String[] args) throws Exception {
         String dataDirectory = "src/main/resources/CarAccidentData/OriData/videos/";
@@ -61,7 +68,12 @@ public class TrafficAccidentDetector {
 
         //Conduct learning
         System.out.println("Starting training...");
-        net.setListeners(new ScoreIterationListener(1));
+        StatsStorage storage = new InMemoryStatsStorage();
+        UIServer server = UIServer.getInstance();
+        server.attach(storage);
+        net.setListeners(
+                new ScoreIterationListener(1),
+                new StatsListener(storage, 1));
 
         TrafficAccidentDatasetIterator trafficAccidentDatasetIterator = new TrafficAccidentDatasetIterator(V_WIDTH, V_HEIGHT);
 
@@ -74,8 +86,10 @@ public class TrafficAccidentDetector {
 //            }
 //        }
 
+
+
         for (int i = 0; i < nTrainEpochs; i++) {
-            DataSetIterator trainData = trafficAccidentDatasetIterator.getDataSetIterator(dataDirectory, labelDirectory, 0, trainMaxIndex+1, miniBatchSize, 467);
+            DataSetIterator trainData = trafficAccidentDatasetIterator.getDataSetIterator(dataDirectory, labelDirectory, 0, trainMaxIndex+1, miniBatchSize, FRAME_LENGTH);
             while(trainData.hasNext()) {
                 System.out.println("@@@next batch");
                 net.fit(trainData.next());
@@ -91,61 +105,42 @@ public class TrafficAccidentDetector {
 
     private static Model getFromScratchModel() {
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-                .seed(seed)
+//                .seed(seed)
                 .l2(l2Coeff) //l2 regularization on all layers
                 .updater(new Adam(learningRate))
+                .weightInit(WeightInit.XAVIER)
+                .activation(Activation.LEAKYRELU)
                 .list()
                 .layer(new ConvolutionLayer.Builder(3, 3)
                         .nIn(3) //3 channels: RGB
                         .nOut(30)
                         .stride(1, 1)
                         .padding(1, 1)
-                        .activation(Activation.RELU)
-                        .weightInit(WeightInit.RELU)
-                        .build())   //Output: (224-3+2)/1+1 = 224 -> 224*224*30
+                        .activation(Activation.LEAKYRELU)
+                        .build())   //Output: (130-3+2)/1+1 = 224 -> 130*130*30
                 .layer(new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
                         .kernelSize(2, 2)
                         .stride(2, 2)
-                        .build())   //(224-2+0)/2+1 = 112 -> 112*112*30
+                        .build())   //(224-2+0)/2+1 = 112 -> 65*65*30
                 .layer(new ConvolutionLayer.Builder(3, 3)
                         .nIn(30)
                         .nOut(10)
                         .stride(1, 1)
                         .padding(1, 1)
-                        .activation(Activation.RELU)
-                        .weightInit(WeightInit.RELU)
-                        .build())   //Output: (112-3+2)/1+1 = 112 -> 112*112*10
-                .layer(new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
-                        .kernelSize(2, 2)
-                        .stride(2, 2)
-                        .build()) //Output: (112-2+0)/2+1 = 56 -> 56*56*10 = 31360
-                .layer(new ConvolutionLayer.Builder(3, 3)
-                        .nIn(10)
-                        .nOut(5)
-                        .stride(1, 1)
-                        .padding(1, 1)
-                        .activation(Activation.RELU)
-                        .weightInit(WeightInit.RELU)
-                        .build())   //Output: (56-3+2)/1+1 = 56 -> 56*56*5
-                .layer(new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
-                        .kernelSize(2, 2)
-                        .stride(2, 2)
-                        .build()) //Output: (56-2+0)/2+1 = 56 -> 28*28*5 = 3920
+                        .activation(Activation.LEAKYRELU)
+                        .build())   //Output: (112-3+2)/1+1 = 112 -> 65*65*10
                 .layer(new DenseLayer.Builder()
-                        .nIn(3920)
+                        .nIn(42250)
                         .nOut(50)
-                        .activation(Activation.RELU)
-                        .weightInit(WeightInit.RELU)
+                        .activation(Activation.LEAKYRELU)
                         .gradientNormalization(GradientNormalization.ClipElementWiseAbsoluteValue)
                         .gradientNormalizationThreshold(10)
-                        .updater(new AdaGrad(0.01))
                         .build())
                 .layer(new LSTM.Builder()
-                        .activation(Activation.TANH)
+                        .activation(Activation.LEAKYRELU)
                         .nIn(50)
                         .nOut(50)
                         .weightInitRecurrent(WeightInit.XAVIER)
-                        .updater(new AdaGrad(0.008))
                         .gradientNormalization(GradientNormalization.ClipElementWiseAbsoluteValue)
                         .gradientNormalizationThreshold(10)
                         .build())
@@ -153,13 +148,12 @@ public class TrafficAccidentDetector {
                         .activation(Activation.SIGMOID)
                         .nIn(50)
                         .nOut(1)    // either accident or not
-                        .weightInit(WeightInit.XAVIER)
                         .gradientNormalization(GradientNormalization.ClipElementWiseAbsoluteValue)
                         .gradientNormalizationThreshold(10)
                         .build())
                 .inputPreProcessor(0, new RnnToCnnPreProcessor(V_HEIGHT, V_WIDTH, nChannels))
-                .inputPreProcessor(6, new CnnToFeedForwardPreProcessor(28, 28, 5))
-                .inputPreProcessor(7, new FeedForwardToRnnPreProcessor())
+                .inputPreProcessor(3, new CnnToFeedForwardPreProcessor(65, 65, 10))
+                .inputPreProcessor(4, new FeedForwardToRnnPreProcessor())
                 .backpropType(BackpropType.TruncatedBPTT)
                 .tBPTTForwardLength(truncatedBPTTLength)
                 .tBPTTBackwardLength(truncatedBPTTLength)
@@ -179,7 +173,7 @@ public class TrafficAccidentDetector {
         System.out.println(resnet50.summary());
 
         FineTuneConfiguration fineTuneConfig = new FineTuneConfiguration.Builder()
-                .seed(seed)
+//                .seed(seed)
                 .l2(0.001) //l2 regularization on all layers
                 .updater(new AdaGrad(learningRate))
                 .weightInit(WeightInit.XAVIER)
@@ -215,33 +209,8 @@ public class TrafficAccidentDetector {
                         "lstm1")
                 .setOutputs("rnnOutput")
                 .build();
+        resnet50Transfer.getConfiguration().addPreProcessors(InputType.convolutional(V_HEIGHT, V_WIDTH, nChannels));
 
-        new ComputationGraphConfiguration.GraphBuilder(resnet50Transfer.getConfiguration(),
-                new NeuralNetConfiguration.Builder(resnet50Transfer.conf()));
-//        ComputationGraph resnet50Transfer = new TransferLearning.GraphBuilder(resnet50)
-//                .fineTuneConfiguration(fineTuneConfig)
-//                .setFeatureExtractor("flatten_1")
-//                .addLayer("lstm1", new LSTM.Builder()
-//                                .activation(Activation.TANH)
-//                                .nIn(1000)
-//                                .nOut(100)
-//                                .weightInit(WeightInit.XAVIER)
-//                                .updater(new AdaGrad(0.008))
-//                                .gradientNormalization(GradientNormalization.ClipElementWiseAbsoluteValue)
-//                                .gradientNormalizationThreshold(10)
-//                                .build(),
-//                        new FeedForwardToRnnPreProcessor(),
-//                        "fc1000")
-//                .addLayer("rnnOutput", new RnnOutputLayer.Builder(LossFunctions.LossFunction.XENT)
-//                                .activation(Activation.SIGMOID)
-//                                .nIn(100)
-//                                .nOut(1)    // either an accident or not
-//                                .gradientNormalization(GradientNormalization.ClipElementWiseAbsoluteValue)
-//                                .gradientNormalizationThreshold(10)
-//                                .build(),
-//                        "lstm1")
-//                .setOutputs("rnnOutput")
-//                .build();
         System.out.println(resnet50Transfer.summary());
 
         return resnet50Transfer;
@@ -254,7 +223,7 @@ public class TrafficAccidentDetector {
         labelMap.put(1, "accident");
         Evaluation evaluation = new Evaluation(labelMap);
 
-        DataSetIterator testData = trafficAccidentDatasetIterator.getDataSetIterator(outputDirectory, outputLabelDirectory, testStartIdx, nExamples, miniBatchSize, 467);
+        DataSetIterator testData = trafficAccidentDatasetIterator.getDataSetIterator(outputDirectory, outputLabelDirectory, testStartIdx, nExamples, miniBatchSize, FRAME_LENGTH);
 
         while(testData.hasNext()) {
             DataSet dsTest = testData.next();
@@ -262,6 +231,28 @@ public class TrafficAccidentDetector {
             System.out.println(predicted);
             evaluation.evalTimeSeries(dsTest.getLabels(), predicted);
         }
+
+        System.out.println(evaluation.stats());
+    }
+
+    private static void evaluatePerformance(TrafficAccidentDatasetIterator trafficAccidentDatasetIterator, ComputationGraph net, int testStartIdx, int nExamples, String outputDirectory, String outputLabelDirectory, int miniBatchSize) throws Exception {
+        //Assuming here that the full test data set doesn't fit in memory -> load 10 examples at a time
+        Map<Integer, String> labelMap = new HashMap<>();
+        labelMap.put(0, "no_accident");
+        labelMap.put(1, "accident");
+        Evaluation evaluation = new Evaluation(labelMap);
+
+        DataSetIterator testData = trafficAccidentDatasetIterator.getDataSetIterator(outputDirectory, outputLabelDirectory, testStartIdx, nExamples, miniBatchSize, FRAME_LENGTH);
+
+        INDArray[] output = net.output(testData);
+        System.out.println(output);
+
+//        while(testData.hasNext()) {
+//            DataSet dsTest = testData.next();
+//            INDArray predicted = net.output(false, dsTest.getFeatures());
+//            System.out.println(predicted);
+//            evaluation.evalTimeSeries(dsTest.getLabels(), predicted);
+//        }
 
         System.out.println(evaluation.stats());
     }
